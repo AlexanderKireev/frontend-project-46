de0d792d1e60d2b5c273ef31071f0eb76a1eccd4
alexander <alexanderkir1980@gmail.com>
2023-11-02 16:32:37 +0300

    update genfiff.js sreate index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { program } from 'commander';
+import genDiff from '../src/index.js';
 
-const program = new Command();
+// const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .parse();
 
-program.parse();
+const { format } = program.opts();
+const [filePath1, filePath2] = program.args;
+
+console.log(genDiff(filePath1, filePath2, format));
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const genDiff = (file1, file2, format) => {
+  return file1 + file2 + format;
+
+};
+
+export default genDiff;
