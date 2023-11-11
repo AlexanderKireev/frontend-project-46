eb09432b50bee096bf7a785044d338722745e275
alexander <alexanderkir1980@gmail.com>
2023-11-11 19:19:53 +0300

    edit fileUtils.js

--- src/fileUtils.js
+++ src/fileUtils.js
@@ -1,17 +1,16 @@
 import { readFileSync } from 'fs';
 import path from 'path';
 import { cwd } from 'process';
-// import parse from './parserFactory.js';
 
 const readFile = (filePath) => {
   const absolutePath = path.resolve(cwd(), filePath);
-  let fileData = null;
   try {
-    fileData = readFileSync(absolutePath, 'utf-8');
+    readFileSync(absolutePath, 'utf-8');
   } catch {
     throw new Error(`File '${filePath}' not found!`);
     // console.error('file not found');
   }
+  const fileData = readFileSync(absolutePath, 'utf-8');
   return fileData;
 };
 
